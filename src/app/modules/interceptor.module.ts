19027bd69e70d18c66668ef45d1faeb90656fb21
Hiep Tran <tranvhiep@gmail.com>
2019-03-28 23:19:57 +0700

    add interceptor module and jwt service

--- src/app/modules/interceptor.module.ts
+++ src/app/modules/interceptor.module.ts
@@ -5,15 +5,37 @@ import {
   HttpHandler,
   HttpEvent,
   HTTP_INTERCEPTORS,
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { JwtService } from '../services';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor(private jwtService: JwtService) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const token = this.jwtService.getToken();
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+    req = req.clone({ setHeaders: headers });
+
+    if (
+      token &&
+      req.method !== 'GET' &&
+      req.method !== 'OPTIONS' &&
+      req.method !== 'HEAD'
+    ) {
+      req = req.clone({
+        headers: req.headers.append('Authorization', `Token ${token}`),
+      });
+    }
+
     return next.handle(req);
   }
 }
---
+++ src/app/services/jwt.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtService {
+
+  constructor() { }
+
+  getToken() {
+    return localStorage.getItem('jwt');
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('jwt', token);
+  }
+
+  destroy() {
+    localStorage.removeItem('jwt');
+  }
+}
